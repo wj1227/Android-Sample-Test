89df2c4c55b5ef9a59c080acd4f57a8a6185fa5c
wj1227 <wj122791@gmail.com>
2020-12-12 15:44:37 +0900

    calendar activity

--- app/src/main/java/com/jay/androidallsampletest/MaterialCalendarViewActivity.kt
+++ app/src/main/java/com/jay/androidallsampletest/MaterialCalendarViewActivity.kt
@@ -1,69 +1,107 @@
 package com.jay.androidallsampletest
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.applandeo.materialcalendarview.CalendarView
 import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 class MaterialCalendarViewActivity : AppCompatActivity() {
     private lateinit var calendarView: CalendarView
+    private lateinit var linearLayout: LinearLayout
     private lateinit var events: MutableList<EventDay>
-
+    private val TAG = javaClass.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_material_calendar_view)
 
-        init()
-        dotEvent()
-        rangeMonth()
-        disableCalendar()
+        initView()
+        initRangeMonth()
+        initDotList()
+        initInvisibleDays()
+        initCalendarClickListener()
     }
 
-    private fun init() {
-        calendarView = findViewById(R.id.calendarView)
+    private fun initView() {
+        linearLayout = findViewById(R.id.linear_layout)
+        calendarView = findViewById(R.id.calendar_view)
         events = mutableListOf()
     }
 
-    private fun dotEvent() {
-        val dot = Calendar.getInstance()
-        dot.add(Calendar.DAY_OF_MONTH, 10)
-        events.add(EventDay(dot, R.drawable.sample_icon_2))
+    private fun initDotList() {
+        val calendar1 = Calendar.getInstance()
+        calendar1.add(Calendar.DAY_OF_MONTH, 3)
+
+        val calendar2 = Calendar.getInstance()
+        calendar2.add(Calendar.DAY_OF_MONTH, 5)
+
+        val calendar3 = Calendar.getInstance()
+        calendar3.add(Calendar.DAY_OF_MONTH, 7)
+
+        events.add(EventDay(calendar1, R.drawable.sample_icon_1))
+        events.add(EventDay(calendar2, R.drawable.sample_icon_2))
+        events.add(EventDay(calendar3, R.drawable.sample_icon_3))
 
         calendarView.setEvents(events)
     }
 
-    private fun rangeMonth() {
+    private fun initRangeMonth() {
         val min = Calendar.getInstance()
-        min.add(Calendar.MONTH, -2)
+        min.add(Calendar.DAY_OF_MONTH, -1)
 
         val max = Calendar.getInstance()
-        max.add(Calendar.MONTH, 2)
+        max.add(Calendar.MONTH, 1)
 
         calendarView.setMinimumDate(min)
         calendarView.setMaximumDate(max)
     }
 
-    private fun disableCalendar() {
-        calendarView.setDisabledDays(disabledDays = disableDays())
-    }
-
-    private fun disableDays(): List<Calendar> {
+    private fun initInvisibleDays() {
         val calendars = mutableListOf<Calendar>()
 
-        val firstDisabled = Calendar.getInstance()
-        firstDisabled.add(Calendar.DAY_OF_MONTH, 2)
+        val first = Calendar.getInstance()
+        first.add(Calendar.DAY_OF_MONTH, 1)
 
-        val secondDisabled = Calendar.getInstance()
-        secondDisabled.add(Calendar.DAY_OF_MONTH, 5)
+        val second = Calendar.getInstance()
+        second.add(Calendar.DAY_OF_MONTH, 4)
 
-        val thirdDisabled = Calendar.getInstance()
-        thirdDisabled.add(Calendar.DAY_OF_MONTH, 8)
+        val third = Calendar.getInstance()
+        third.add(Calendar.DAY_OF_MONTH, 6)
 
-        calendars.add(firstDisabled)
-        calendars.add(secondDisabled)
-        calendars.add(thirdDisabled)
+        val fourth = Calendar.getInstance()
+        fourth.add(Calendar.DAY_OF_MONTH, 12)
 
-        return calendars
+        val fifth = Calendar.getInstance()
+        fifth.add(Calendar.DAY_OF_MONTH, 15)
+
+        val final = Calendar.getInstance()
+        final.add(Calendar.DAY_OF_MONTH, 27)
+
+        calendars.add(first)
+        calendars.add(second)
+        calendars.add(third)
+        calendars.add(fourth)
+        calendars.add(fifth)
+        calendars.add(final)
+
+        calendarView.setDisabledDays(calendars)
     }
+
+    private fun initCalendarClickListener() {
+        calendarView.setOnDayClickListener(object : OnDayClickListener {
+            override fun onDayClick(eventDay: EventDay) {
+                val day = "${eventDay.calendar[Calendar.YEAR]}년 " +
+                        "${eventDay.calendar[Calendar.MONTH].plus(1)}월 " +
+                        "${eventDay.calendar[Calendar.DAY_OF_MONTH]}일"
+                Snackbar.make(linearLayout, day, 700).also { snackbar ->
+                    snackbar.setAction("확인") { snackbar.dismiss() }
+                }.show()
+            }
+        })
+    }
+
 }
