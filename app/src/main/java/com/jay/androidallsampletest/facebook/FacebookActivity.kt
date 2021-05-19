657f7c84babea4a29c373fb75d80bd792f3f7c4d
wj <wj122791@gmail.com>
2021-05-19 22:29:47 +0900

    [ 페이스북 로그인 완성]

--- app/src/main/java/com/jay/androidallsampletest/facebook/FacebookActivity.kt
+++ app/src/main/java/com/jay/androidallsampletest/facebook/FacebookActivity.kt
@@ -3,9 +3,8 @@ package com.jay.androidallsampletest.facebook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
+import com.facebook.*
+import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
 import com.jay.androidallsampletest.R
@@ -21,10 +20,22 @@ class FacebookActivity : AppCompatActivity() {
         callbackManager = CallbackManager.Factory.create()
         button = findViewById(R.id.btn_login)
 
+
+        val token = AccessToken.getCurrentAccessToken()
+        if (token != null && !token.isExpired) {
+            println("토큰이 유효하고 널이 아님")
+        } else {
+            println("토큰 없거나 유효하지 않음, 테스트 자동조회")
+            //LoginManager.getInstance().logInWithPublishPermissions(this, mutableListOf())
+            LoginManager.getInstance().logIn(this, mutableListOf("public_profile"))
+        }
+
         button.apply {
             registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
                 override fun onSuccess(result: LoginResult?) {
                     println("onSuccess: ${result?.accessToken},\n${result}")
+                    val result = FacebookMapper.mapper(Profile.getCurrentProfile())
+                    println("model result: $result")
                 }
 
                 override fun onCancel() {
@@ -42,5 +53,39 @@ class FacebookActivity : AppCompatActivity() {
         println("result!")
         callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+    }
+}
+
+/**
+ * id String
+ * firstName String,
+ * middleName String,
+ * lastName String,
+ * name String,
+ * link Uri
+ */
+data class Model(
+    val id: String? = null,
+    val firstName: String? = null,
+    val middleName: String? = null,
+    val lastName: String? = null,
+    val name: String? = null,
+    val profileImage: String? = null
+)
+
+interface Mapper<in D : Profile, out R : Model> {
+    fun mapper(item: D): R
+}
+
+object FacebookMapper : Mapper<Profile, Model> {
+    override fun mapper(item: Profile): Model {
+        return Model(
+            id = item.id,
+            firstName = item.firstName,
+            middleName = item.middleName,
+            lastName = item.lastName,
+            name = item.name,
+            profileImage = item.linkUri.toString()
+        )
     }
 }
