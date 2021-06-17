62fd782c6e8358fe7c0a7fda81761d9a1520a5ca
wj <wj122791@gmail.com>
2021-06-17 23:08:54 +0900

    Feat [ Reply Notification ]

--- app/src/main/java/com/jay/androidallsampletest/notification/NotificationActivity.kt
+++ app/src/main/java/com/jay/androidallsampletest/notification/NotificationActivity.kt
@@ -1,22 +1,22 @@
 package com.jay.androidallsampletest.notification
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
 import com.jay.androidallsampletest.R
 import com.jay.androidallsampletest.toolbartest.ToolbarTestActivity
 
 class NotificationActivity : AppCompatActivity() {
+    private val TAG = javaClass.simpleName
 
     private val createNotification: Button by lazy {
         findViewById(R.id.btn_create_notification)
@@ -28,6 +28,10 @@ class NotificationActivity : AppCompatActivity() {
 
     private val actionNotification: Button by lazy {
         findViewById(R.id.btn_notification_btn_add)
+    }
+
+    private val replyNotification: Button by lazy {
+        findViewById(R.id.btn_reply_notification)
     }
 
 //    private val receiver: JayBroadcastReceiver by lazy {
@@ -48,6 +52,11 @@ class NotificationActivity : AppCompatActivity() {
 
         initClickListener()
         createChannel()
+
+        val text = getMessageText(intent)
+        if (!text.isNullOrEmpty()) {
+            Toast.makeText(this, "$text", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStart() {
@@ -70,6 +79,9 @@ class NotificationActivity : AppCompatActivity() {
         actionNotification.setOnClickListener {
             action()
         }
+        replyNotification.setOnClickListener {
+            reply()
+        }
     }
 
     private fun create() {
@@ -87,7 +99,12 @@ class NotificationActivity : AppCompatActivity() {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
 
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            0
+        )
 
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_arrow_right)
@@ -106,7 +123,12 @@ class NotificationActivity : AppCompatActivity() {
             putExtra("notifi", 9999)
         }
 
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.sample_icon_3)
@@ -116,6 +138,47 @@ class NotificationActivity : AppCompatActivity() {
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .addAction(R.drawable.ic_launcher_foreground, "add button", pendingIntent)
+
+        notify(builder)
+    }
+
+    private fun reply() {
+        val replyLabel = "please your message"
+        val remoteInput: RemoteInput = RemoteInput.Builder(KEY_TEXT_REPLY).run {
+            setLabel(replyLabel)
+            build()
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val action: NotificationCompat.Action = NotificationCompat.Action.Builder(
+            0,
+            "TITLE!",
+            pendingIntent
+        )
+            .addRemoteInput(remoteInput)
+            .build()
+
+        val action2: NotificationCompat.Action = NotificationCompat.Action.Builder(
+            R.drawable.ic_launcher_foreground,
+            getString(R.string.app_name),
+            pendingIntent
+        )
+            .addRemoteInput(remoteInput)
+            .build()
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_arrow_left)
+            .setContentTitle("reply title")
+            .setContentText("reply description!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .addAction(action)
+            .addAction(action2)
 
         notify(builder)
     }
@@ -140,8 +203,14 @@ class NotificationActivity : AppCompatActivity() {
         }
     }
 
+    private fun getMessageText(intent: Intent): CharSequence? {
+        return RemoteInput.getResultsFromIntent(intent)?.getCharSequence(KEY_TEXT_REPLY)
+    }
+
     companion object {
         const val CHANNEL_ID = "1"
         const val NOTIFICATION_ID = 100
+
+        const val KEY_TEXT_REPLY = "key_text_reply"
     }
 }
